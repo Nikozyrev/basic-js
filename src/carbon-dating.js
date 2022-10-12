ab8d0c6e2877e91125414097a3e9fbb880315592
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2022-10-12 14:19:02 +0300

    feat: implement Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(activity) {
+    if (typeof activity !== 'string') {
+      return false;
+    }
+    const sampleActivity = Number(activity);
+    if (Number.isNaN(sampleActivity) || sampleActivity <= 0 || sampleActivity >= 15) {
+      return false;
+    }
+    const ln = Math.log(MODERN_ACTIVITY/sampleActivity);
+    const k = 0.693 / HALF_LIFE_PERIOD;
+    return Math.ceil(ln / k);
 }
 
 module.exports = {
