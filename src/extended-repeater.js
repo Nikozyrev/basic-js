4608d55335346264bf01808d5054b8ba585130b7
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2022-10-13 17:32:22 +0300

    feat: implement extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+    const string = `${str}`;
+    const repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+    const separator = options.separator ? options.separator : '+';
+    const addition = options.hasOwnProperty('addition') ? (`${options.addition}` ) : '';
+    const additionRepeatTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+    const additionSeparator = options.additionSeparator ? options.additionSeparator : '|';
+
+    function repeatStr(str, repeatTimes, separator) {
+        let result = str;
+        for (let i = 0; i < repeatTimes - 1; i++) {
+            result = result + separator + str; 
+        }
+        return result;
+    }
+
+    const repeatedAddition = repeatStr(addition, additionRepeatTimes, additionSeparator);
+    const stringWithAddition = string + repeatedAddition;
+    return repeatStr(stringWithAddition, repeatTimes, separator);
 }
 
 module.exports = {
