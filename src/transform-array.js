c84818587df5a78973a20a9fb276b2e9ef95aa69
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2022-10-14 14:06:34 +0300

    feat: implement transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error(`'arr' parameter must be an instance of the Array!`);
+    const controls = {
+      discard_next: "--discard-next",
+      discard_prev: "--discard-prev",
+      double_next: "--double-next",
+      double_prev: "--double-prev",
+    }
+    const controlsValues = Object.values(controls);
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (controlsValues.find(el => el === arr[i])) {
+            if (arr[i] === controls.discard_next) {
+                arr.splice(i+1, 1);
+            }
+            if (arr[i] === controls.discard_prev) {
+                if (!controlsValues.find(el => el === arr[i-1])) result.pop();
+            }
+            if (arr[i] === controls.double_next) {
+                arr[i+1] ? result.push(arr[i+1]) : false;
+            }
+            if (arr[i] === controls.double_prev) {
+                if (arr[i-1] && !controlsValues.find(el => el === arr[i-1])) {
+                  result.push(arr[i-1])
+                }
+            }
+        }
+        if (!controlsValues.find(el => el === arr[i])) {
+          result.push(arr[i]);
+        }
+    }
+    return result;
 }
 
 module.exports = {
